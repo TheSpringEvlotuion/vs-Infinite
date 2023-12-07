f6fb391bc0fab158ce4727cd3fb241e396eeaba2
TheSpringEvlotuion <96666657+TheSpringEvlotuion@users.noreply.github.com>
2023-12-07 18:28:55 -0300

    Add files via upload

--- assets/preload/data/masked/script.lua
+++ assets/preload/data/masked/script.lua
@@ -1,12 +1,12 @@
 function onCreate()
 	posX = 0;
 	posY = 0;
-	scale = 1;
+	scale = 2;
 	
 	makeAnimatedLuaSprite('sonicspeed', 'SSH/sonicspeed', posX, posY);
 	addAnimationByPrefix('sonicspeed', 'sonicwin', 'BlueSpeed', 24, true);
 	setScrollFactor('sonicspeed', 0, 0);
-	scaleObject('sonicspeed', 1.5, 1.5);
+	scaleObject('sonicspeed', 3.0, 3.0);
 	setProperty('sonicspeed.alpha', 0.001);
 	addLuaSprite('sonicspeed', false);
     screenCenter('sonicspeed');
